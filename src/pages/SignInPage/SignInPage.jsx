da61595892c90b18a3ef791daa35bc1b53cc6b19
thinhhung <nu1412ti@gmail.com>
2025-03-05 20:07:54 +0700

    test push code dat -> hung

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,4 +1,13 @@
-import { Button, Input,Form, notification, Row, Col, Divider, message } from "antd"
+import {
+  Button,
+  Input,
+  Form,
+  notification,
+  Row,
+  Col,
+  Divider,
+  message,
+} from "antd";
 
 import { Link, useNavigate } from "react-router-dom";
 import { ArrowRightOutlined } from "@ant-design/icons";
@@ -6,108 +15,103 @@ import { useState } from "react";
 import { loginApi } from "../../services/api.service";
 
 const LoginForm = () => {
-    const [form] = Form.useForm();
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-    const onFinish = async (values) => {
-      const { email, password } = values;
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const onFinish = async (values) => {
+    const { email, password } = values;
+
+    setLoading(true);
+    const res = await loginApi(email, password);
+    if (res.data && res.data.Error) {
+      notification.error({
+        message: "Error Login",
+        description: JSON.stringify(res.message),
+      });
+
+      navigate("/login");
       
-   setLoading(true)
-      const res = await loginApi(email,password);
-        if (res.data) {
-message.success("Đăng nhập thành công")
-navigate("/");
-        }else {
-          notification.error({
-            message   :"Error Login",
-            description: JSON.stringify(res.message)
-          })
-          navigate("/login");
-          }
-          setLoading(false)
-        }
+    } else {
+      message.success("Đăng nhập thành công");
+      navigate("/");
+    }
+    setLoading(false);
+  };
   return (
-    <Row justify={"center"} style={{marginTop: "30px"}}>
+    <Row justify={"center"} style={{ marginTop: "30px" }}>
       <Col xs={24} md={8}>
-    <fieldset style={{
-      padding: "15px",
-      margin: "5px",
-      border: "1px solid #ccc",
-      borderRadius: "5px"
-    }}>
-    <legend>Đăng Nhập</legend>
+        <fieldset
+          style={{
+            padding: "15px",
+            margin: "5px",
+            border: "1px solid #ccc",
+            borderRadius: "5px",
+          }}>
+          <legend>Đăng Nhập</legend>
 
-      <Form
-      form={form} 
-      layout="vertical"
-     
-        onFinish={onFinish}
-        style={{ margin: "10px" }}
-      
-      //   onFinishFailed={onFinishFailed}
-     
-  >
-  
-    
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: 'Email không đc để trống',
-          },
-          {
-            type: "email",
-            message: 'Email không đúng định dạng',
-          }
-        ]}
-      >
-        <Input />
-      </Form.Item>
-     
-      
-  
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Passwd không đc để trống',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-   
-  
-  
+          <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+            style={{ margin: "10px" }}
 
-  
-   
-        <div style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center'
+            //   onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Email không đc để trống",
+                },
+                {
+                  type: "email",
+                  message: "Email không đúng định dạng",
+                },
+              ]}>
+              <Input />
+            </Form.Item>
 
-        }}>
-          <Button 
-          loading={loading}
-           onClick={() => form.submit()}
-          type="primary" > Login </Button>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Passwd không đc để trống",
+                },
+              ]}>
+              <Input.Password />
+            </Form.Item>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}>
+              <Button
+                loading={loading}
+                onClick={() => form.submit()}
+                type="primary">
+                {" "}
+                Login{" "}
+              </Button>
+            </div>
+            <Link to="/">
+              Go to home <ArrowRightOutlined />
+            </Link>
+          </Form>
+          <Divider />
+          <div style={{ textAlign: "center" }}>
+            Chưa có tài khoản <Link to={"registers"}>Đăng ký tại đây </Link>
           </div>
-       <Link to="/">Go to home <ArrowRightOutlined/></Link>
-   
-   
-     
-      </Form>
-      <Divider />
-      <div style={{ textAlign: 'center' }}>Chưa có tài khoản <Link to={"registers"}>Đăng ký tại đây </Link></div>
-      </fieldset>
+        </fieldset>
       </Col>
-      </Row>
-  )
-  }
+    </Row>
+  );
+};
+//test hihi
 
-export default LoginForm
+export default LoginForm;
